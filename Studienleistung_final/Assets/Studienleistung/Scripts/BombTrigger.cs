d3cdabc7a207c384e21eb9611e44d1bba583ccd9
Julesn <Julia.Sageder@stud.uni-regensburg.de>
2016-05-24 23:39:10 +0200

    update

---
+++ Studienleistung_final/Assets/Studienleistung/Scripts/BombTrigger.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BombTrigger : MonoBehaviour {
+
+	bool inZone;
+	Bomb bomb;
+
+	void Update(){
+		if (inZone == true) {
+				if (bomb.bombStatus == false) {
+					bomb.bombStatus = true;
+				} else {
+				bomb.bombStatus = false;
+				}
+		}
+	}
+
+	void Start(){
+		bomb = transform.parent.parent.GetComponentInChildren<Bomb>();
+	}
+
+	void OnTriggerEnter(Collider other){
+		Debug.Log ("TriggerEnter");
+		inZone = true;
+	}
+
+	void OnTriggerExit(Collider other){
+		Debug.Log ("TriggerExit");
+		inZone = false;
+	}
+}
---
+++ Studienleistung_final/Assets/Studienleistung/Scripts/Door.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Door : MonoBehaviour {
+
+	bool isOpen;
+	float doorPosOpen = 1.5f;
+	float doorPosClosed = 3f;
+	float yPos;
+
+	public bool doorStatus {
+		get {
+			return isOpen;
+		}
+		set { 
+			isOpen = value;
+		}
+	}
+
+	void Update(){
+		yPos = transform.position.y;
+		if (isOpen == true) {
+			if (yPos > doorPosOpen) {
+				Vector3 open = new Vector3 (0, -doorPosOpen, 0);
+				transform.Translate (open * Time.deltaTime, Space.World);
+			}
+		}
+		if (isOpen == false) {
+			if (yPos < doorPosClosed) {
+				Vector3 close = new Vector3 (0, doorPosClosed, 0);
+				transform.Translate (close * Time.deltaTime, Space.World);
+			}
+		}
+	}
+		
+}
