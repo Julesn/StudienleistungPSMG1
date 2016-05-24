ece3ec03781de85a7fc2a2ea8db5359c1c4157a3
Julesn <Julia.Sageder@stud.uni-regensburg.de>
2016-05-24 22:39:56 +0200

    Erster Commit

---
+++ Studienleistung/Assets/Studienleistung/Scripts/Bomb.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bomb : MonoBehaviour {
+
+	bool trappedIn;
+	float bombPosActive = 1.5f;
+	float bombPosInactive = 3f;
+	float yPos;
+
+	public bool bombStatus {
+		get {
+			return trappedIn;
+		}
+		set { 
+			trappedIn = value;
+		}
+	}
+
+	void Update(){
+		yPos = transform.position.y;
+		if (trappedIn == true) {
+			if (yPos > bombPosActive) {
+				Vector3 open = new Vector3 (0, -bombPosActive, 0);
+				transform.Translate (open * Time.deltaTime, Space.World);
+			}
+		}
+		if (trappedIn == false) {
+			if (yPos < bombPosInactive) {
+				Vector3 close = new Vector3 (0, bombPosInactive, 0);
+				transform.Translate (close * Time.deltaTime, Space.World);
+			}
+		}
+	}
+}
---
+++ Studienleistung/Assets/Studienleistung/Scripts/PlayerManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerManager : MonoBehaviour {
+
+	public int m_Health = 3;
+
+	public float m_lessHealthLessSpeed = 2f;
+
+	void decreaseHealth(){
+		if (m_Health == 2) {
+
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
